fb2ff906397197e7587f1d16d02bb785d11dba28
Arnav-bhoria <82876881+Arnav-bhoria@users.noreply.github.com>
2022-02-11 16:34:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//https://teachablemachine.withgoogle.com/models/1hdocpWXo/
+Webcam.set({
+height:300,
+width:350,
+image_format:'png',
+png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach(
+"#camera"
+)
+
+
